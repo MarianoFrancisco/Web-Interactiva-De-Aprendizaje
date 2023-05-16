f7cceb54967d9a792fbec86db65c093f023ed907
Manuel Rojas <manuerjs70@gmail.com>
2023-05-16 17:22:40 -0600

    feat: add auth, jwt and user controller

---
+++ Backend/app/controllers/userController.js
@@ -0,0 +1,68 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    if (!users) return res.status(204).json({ 'message': 'No users found' });
+    res.json(users);
+}
+
+const createNewUser = async (req, res) => {
+    const { username, password, fullname, roles, email, birthdayDate } = req.body;
+    if (!username || !password || !fullname || !roles || !email || !birthdayDate) return res.status(400).json({ 'message': 'Username, password and fullname are required.' });
+
+    // check for duplicate usernames in the db
+    const duplicate = await User.findOne({ username }).exec();
+    if (duplicate) return res.sendStatus(409); //Conflict 
+
+    try {
+        //encrypt the password
+        const hashedPwd = await bcrypt.hash(password, 10);
+
+        //create and store the new user
+        const result = await User.create({
+            email,
+            fullname,
+            username,
+            "password": hashedPwd,
+            roles,
+            birthdayDate
+        });
+
+        console.log(result);
+
+        res.status(201).json({ 'success': `New user ${username} created!` });
+    } catch (err) {
+        res.status(500).json({ 'message': err.message });
+    }
+}
+
+const updateUser = async (req, res) => {
+
+}
+
+const deleteUser = async (req, res) => {
+    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findByIdAndDelete(req.body.id)
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
+    }
+    res.json(user);
+}
+
+const getUser = async (req, res) => {
+    if (!req?.params?.id) return res.status(400).json({ "message": 'User ID required' });
+    const user = await User.findOne({ _id: req.params.id }).exec();
+    if (!user) {
+        return res.status(204).json({ 'message': `User ID ${req.params.id} not found` });
+    }
+    res.json(user);
+}
+
+module.exports = {
+    getAllUsers,
+    createNewUser,
+    updateUser,
+    deleteUser,
+    getUser
+}
