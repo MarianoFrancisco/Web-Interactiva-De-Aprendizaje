9e5a1905b2c2b2ff5c699afac67aa70c51d4e29f
Manuel Rojas <manuerjs70@gmail.com>
2023-05-16 18:15:00 -0600

    add first implementation of verifyRoles

--- Backend/app/routes/user.routes.js
+++ Backend/app/routes/user.routes.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const routes = express.Router();
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../middleware/verifyRoles");
+const verifyJWT = require("../middleware/verifyJWT");
 const {
   createNewUser,
   deleteUser,
@@ -8,10 +11,11 @@ const {
   updateUser,
 } = require("../controllers/userController");
 
-routes.route('/')
-.get(getAllUsers)
-.post(createNewUser)
-.patch(updateUser)
-.delete(deleteUser);
+routes
+  .route("/")
+  .get([verifyJWT, verifyRoles(ROLES_LIST.Admin)], getAllUsers)
+  .post(createNewUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 module.exports = routes;
