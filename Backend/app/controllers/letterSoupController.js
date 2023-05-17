b49cd7f2a21795658dca948f9de45f3e4c50b68f
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-16 19:36:42 -0600

    Rutas de Sopa de Letras agregadas

--- Backend/app/controllers/letterSoupController.js
+++ Backend/app/controllers/letterSoupController.js
@@ -13,6 +13,48 @@ const saveGame = async (req, res) => {
     }
 }
 
+const getGame = async (req, res) => {
+    try {
+        const id_game = req.params.id;
+        const game = await LetterSoup.findById(id_game);
+        if (game) {
+            res.status(200).json(game);
+        } else {
+            res.status(400).json({ error: "Parece que el juego de memoria no existe..." });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Ocurrio un error interno en el servidor...' });
+    }
+}
+
+const editGame = async (req, res) => {
+    try {
+        const id_game = req.body.id_game;
+        const data = req.body.data;
+        const game = await LetterSoup.findByIdAndUpdate(id_game, { data });
+        res.status(200).json(game);
+    } catch (error) {
+        res.status(500).json({ error: 'Ocurrio un error interno en el servidor...' })
+    }
+}
+
+const deleteGame = async (req, res) => {
+    try {
+        const id_game = req.params.id;
+        const game = await LetterSoup.findByIdAndDelete(id_game);
+        if (game) {
+            res.status(200).json(game);
+        } else {
+            res.status(404).json({ error: 'Parece que el juego que intentas eliminar no existe...' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Ocurrio un error interno en el servidor...' });
+    }
+}
+
 module.exports = {
-    saveGame
+    saveGame,
+    getGame,
+    editGame,
+    deleteGame
 }
