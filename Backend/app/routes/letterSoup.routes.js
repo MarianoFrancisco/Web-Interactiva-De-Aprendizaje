f78f6c29bb3cf1b1068bfef5fff6ff056c59c653
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-16 20:09:34 -0600

    Autenticacion agregada

--- Backend/app/routes/letterSoup.routes.js
+++ Backend/app/routes/letterSoup.routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const { insertGame, getGame, editGame, deleteGame } = require('../controllers/letterSoupController');
+const verifyJWT = require('../middleware/verifyJWT');
+const verifyRoles = require('../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/roles_list');
 
 const routes = express.Router();
 routes.post('/add-game', verifyJWT, verifyRoles(ROLES_LIST.Teacher), insertGame);
