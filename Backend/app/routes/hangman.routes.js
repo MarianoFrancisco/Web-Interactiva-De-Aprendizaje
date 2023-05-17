4ba12af262de6c1fb8d2dc47931595bd750652ef
Mariano Camposeco <mariano1941@outlook.es>
2023-05-16 22:30:47 -0600

    Agregamos metodos funcionales para hangman

--- Backend/app/routes/hangman.routes.js
+++ Backend/app/routes/hangman.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { saveGame } = require('../controllers/hangmanController');
+const { saveGame,editGame,deleteGame,getGame } = require('../controllers/hangmanController');
 
 const routes = express.Router();
 routes.post('/saveGame', saveGame);
-
+routes.patch('/editGame', editGame);
+routes.delete('/deleteGame/:id', deleteGame);
+routes.get('/getGame/:id', getGame);
 module.exports = routes;
