3f03d2297ab1b6369a9d255c25d2e5bfd85ad472
Manuel Rojas <manuerjs70@gmail.com>
2023-05-16 18:03:00 -0600

    update user routes

--- Backend/app/routes/user.routes.js
+++ Backend/app/routes/user.routes.js
@@ -1,4 +1,17 @@
-const express = require('express');
+const express = require("express");
 const routes = express.Router();
+const {
+  createNewUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} = require("../controllers/userController");
+
+routes.route('/')
+.get(getAllUsers)
+.post(createNewUser)
+.patch(updateUser)
+.delete(deleteUser);
 
 module.exports = routes;
