51b5f28c67f3948c2071deb92df1ddddbe81f331
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-18 14:00:22 -0600

    Proteccion en la rutas de Game agregadas

--- Backend/app/routes/game.routes.js
+++ Backend/app/routes/game.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { saveGame, getGamesByUser } = require('../controllers/gameController');
+const verifyJWT = require('../middleware/verifyJWT');
+const verifyRoles = require('../middleware/verifyRoles');
+const ROLES_LIST = require('../../config/roles_list');
 
 const routes = express.Router();
-routes.post('/saveGame', saveGame);
-routes.get('/getByUser/:id', getGamesByUser);
+routes.post('/saveGame', verifyJWT, verifyRoles(ROLES_LIST.Teacher), saveGame);
+routes.get('/getByUser/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), getGamesByUser);
 
 module.exports = routes;
