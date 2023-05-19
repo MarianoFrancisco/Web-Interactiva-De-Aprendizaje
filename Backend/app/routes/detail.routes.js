5d8f718485fc6188d2959f4c2b5ff57346cb1f38
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-19 17:27:05 -0600

    Rutas de los detalles actualizadas

--- Backend/app/routes/detail.routes.js
+++ Backend/app/routes/detail.routes.js
@@ -7,7 +7,6 @@ const verifyRoles = require('../middleware/verifyRoles');
 const ROLES_LIST = require('../../config/roles_list');
 
 const routes = express.Router();
-routes.post('/add-detail', insertDetail);
 routes.get('/view-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), getDetail);
 routes.patch('/edit-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), editDetail);
 routes.delete('/delete-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), deleteResult, deleteGame, deleteDetail);
