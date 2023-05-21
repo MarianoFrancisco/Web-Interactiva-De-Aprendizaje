e317da9f753c556eb6485bea6c6415eb4b1ca781
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-20 23:32:13 -0600

    Cambios agregados sobre detalles y games

--- Backend/app/routes/detail.routes.js
+++ Backend/app/routes/detail.routes.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const { insertDetail, getDetail, editDetail, deleteDetail } = require('../controllers/detailController');
-const { deleteGame } = require('../controllers/gameController');
-const { deleteResult } = require('../controllers/resultController');
+const { insertDetail, getDetail, editDetail } = require('../controllers/detailController');
 const verifyJWT = require('../middleware/verifyJWT');
 const verifyRoles = require('../middleware/verifyRoles');
 const ROLES_LIST = require('../../config/roles_list');
@@ -10,6 +8,5 @@ const routes = express.Router();
 routes.post('/', verifyJWT, verifyRoles(ROLES_LIST.Teacher), insertDetail);
 routes.get('/view-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), getDetail);
 routes.patch('/edit-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), editDetail);
-routes.delete('/delete-detail/:id', verifyJWT, verifyRoles(ROLES_LIST.Teacher), deleteResult, deleteGame, deleteDetail);
 
 module.exports = routes;
