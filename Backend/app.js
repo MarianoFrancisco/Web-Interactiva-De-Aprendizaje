61dd2a5a46d8f6766d53654da7e2de078c4f8042
Luis Cifuentes <luiscifuentes202030482@cunoc.edu.gt>
2023-05-22 01:45:19 -0600

    Server agregado al app

--- Backend/app.js
+++ Backend/app.js
@@ -12,6 +12,7 @@ const app = express();
 const body_parser = require("body-parser");
 const routes = require("./app/routes/index");
 const morgan = require("morgan");
+const server = require('./server/server');
 const credentials = require("./app/middleware/credentials");
 const corsOptions = require("./config/corsOptions");
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(body_parser.json());
 app.use(credentials);
 app.use(cors(corsOptions));
+app.use(server);
 //middleware for cookies
 app.use(cookieParser());
 app.use("/api/v1.0", routes);
