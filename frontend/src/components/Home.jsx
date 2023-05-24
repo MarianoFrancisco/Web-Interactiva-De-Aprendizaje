5f932dfafa3bb1af6620bc6158837a8608603f82
Mariano Camposeco <mariano1941@outlook.es>
2023-05-24 15:22:17 -0600

    Apareciencia visual en inicio modificada

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,12 +2,14 @@
 export default function Home() {
   
   return (
-    <><h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-    BIENVENID@, ¿QUÉ PIENSAS HACER HOY?
-  </h2>
+    <><h1 className=" text-center text-5xl font-bold leading-9 text-gray-900 bg-yellow-300">
+    GAME INTERACTIVE GT
+  </h1><br/>
   <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-  <img src={`./FondoHome.jpg`} alt="Posicion" className="w-20 h-20 object-contain" />
-</div>
+  <img src={`./FondoHome.jpg`} alt="Posicion" style={{ width: "1100px", height: "550px" }} />
+</div><h1 className="mt-5 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 bg-blue-300 " >
+    ¿QUÉ PIENSAS HACER HOY?
+  </h1>
     </>
   );
 }
