d4a7d1f5e504aaa96a0637942811c2a20e3aaad0
Manuel Rojas <manuerjs70@gmail.com>
2023-05-24 09:31:15 -0600

    fix update password user controller

--- Backend/app/controllers/userController.js
+++ Backend/app/controllers/userController.js
@@ -1,88 +1,103 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const getAllUsers = async (req, res) => {
-    const users = await User.find();
-    if (!users) return res.status(204).json({ 'message': 'No users found' });
-    res.json(users);
-}
+  const users = await User.find();
+  if (!users) return res.status(204).json({ message: "No users found" });
+  res.json(users);
+};
 
 const createNewUser = async (req, res) => {
-    console.log(req.body)
-    const { username, password, fullname, roles, email, birthdayDate } = req.body;
-    if (!username || !password || !fullname || !roles || !email || !birthdayDate) return res.status(400).json({ 'message': 'Username, password and fullname are required.' });
+  console.log(req.body);
+  const { username, password, fullname, roles, email, birthdayDate } = req.body;
+  if (!username || !password || !fullname || !roles || !email || !birthdayDate)
+    return res
+      .status(400)
+      .json({ message: "Username, password and fullname are required." });
 
-    // check for duplicate usernames in the db
-    const duplicate = await User.findOne({ username }).exec();
-    if (duplicate) return res.sendStatus(409); //Conflict 
+  // check for duplicate usernames in the db
+  const duplicate = await User.findOne({ username }).exec();
+  if (duplicate) return res.sendStatus(409); //Conflict
 
-    try {
-        //encrypt the password
-        const hashedPwd = await bcrypt.hash(password, 10);
+  try {
+    //encrypt the password
+    const hashedPwd = await bcrypt.hash(password, 10);
 
-        //create and store the new user
-        const result = await User.create({
-            email,
-            fullname,
-            username,
-            "password": hashedPwd,
-            roles,
-            birthdayDate
-        });        
+    //create and store the new user
+    const result = await User.create({
+      email,
+      fullname,
+      username,
+      password: hashedPwd,
+      roles,
+      birthdayDate,
+    });
 
-        res.status(201).json({ 'success': `New user ${username} created!` });
-    } catch (err) {
-        res.status(500).json({ 'message': err.message });
-    }
-}
+    res.status(201).json({ success: `New user ${username} created!` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 const updateUser = async (req, res) => {
-    console.log(req.body);
-    if (!req?.body?._id) {
-        return res.status(400).json({ 'message': 'ID parameter is required.' });
-    }
+  console.log(req.body);
+  if (!req?.body?._id) {
+    return res.status(400).json({ message: "ID parameter is required." });
+  }
 
-    const user = await User.findOne({ _id: req.body._id }).exec();
-    if (!user) {
-        return res.status(204).json({ "message": `No user matches ID ${req.body.id}.` });
-    }
-    if (req.body?.fullname) user.fullname = req.body.fullname;
-    if (req.body?.roles) user.roles = req.body.roles;
-    if (req.body?.password) user.password = req.body.password;
-    if (req.body?.email) user.email = req.body.email;
-    if (req.body?.birthdayDate) user.birthdayDate = req.body.birthdayDate;
-    const result = await user.save();
-    console.log(result);
-    res.json(result);
-}
+  const user = await User.findOne({ _id: req.body._id }).exec();
+  if (!user) {
+    return res
+      .status(204)
+      .json({ message: `No user matches ID ${req.body.id}.` });
+  }
+  if (req.body?.fullname) user.fullname = req.body.fullname;
+  if (req.body?.roles) user.roles = req.body.roles;
+  if (req.body?.password) {
+    const hashedPwd = await bcrypt.hash(req.body.password, 10);
+    user.password = hashedPwd;
+  }
+  if (req.body?.email) user.email = req.body.email;
+  if (req.body?.birthdayDate) user.birthdayDate = req.body.birthdayDate;
+  const result = await user.save();
+  console.log(result);
+  res.json(result);
+};
 
 const deleteUser = async (req, res) => {
-    if (!req?.body?.id) return res.status(400).json({ "message": 'User ID required' });
-    const user = await User.findByIdAndDelete(req.body.id)
-    if (!user) {
-        return res.status(204).json({ 'message': `User ID ${req.body.id} not found` });
-    }
-    res.json(user);
-}
+  if (!req?.body?.id)
+    return res.status(400).json({ message: "User ID required" });
+  const user = await User.findByIdAndDelete(req.body.id);
+  if (!user) {
+    return res
+      .status(204)
+      .json({ message: `User ID ${req.body.id} not found` });
+  }
+  res.json(user);
+};
 
 const getUser = async (req, res) => {
-    try {
-        const id = req.userId;
-        const results = await User.find({ _id: id});
-        if (results.length > 0) {
-            res.status(200).json(results[0]);
-        } else {
-            res.status(404).jason({ message: 'No se encontraron resultados de este usuario...' })
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Ocurrio un error interno en el servidor...' });
+  try {
+    const id = req.userId;
+    const results = await User.find({ _id: id });
+    if (results.length > 0) {
+      res.status(200).json(results[0]);
+    } else {
+      res
+        .status(404)
+        .jason({ message: "No se encontraron resultados de este usuario..." });
     }
-}
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Ocurrio un error interno en el servidor..." });
+  }
+};
 
 module.exports = {
-    getAllUsers,
-    createNewUser,
-    updateUser,
-    deleteUser,
-    getUser
-}
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  getUser,
+};
